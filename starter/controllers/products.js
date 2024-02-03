3ce6c396df88076f72b4653f2e690e67ebe52025
Beaskyy <afeez.akintobi@yahoo.com>
2024-02-03 11:44:38 +0100

    products

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,9 +1,33 @@
+const product = require("../models/product");
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: `Products testing routes` });
+  const products = await Product.find({
+    name: "vase table",
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: `Get all products` });
+  const { featured, company, name } = req.query;
+
+  const queryObjects = {};
+
+  if (featured) {
+    queryObjects.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObjects.company = company;
+  }
+
+  if (name) {
+    queryObjects.name = name;
+  }
+
+  console.log(queryObjects);
+  const products = await Product.find(queryObjects);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
